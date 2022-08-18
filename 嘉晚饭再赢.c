1dbf75354043512e0b0cf3c86f0891d34b60f1aa
adeuxySNT07 <110272604+adeuxySNT07@users.noreply.github.com>
2022-08-18 15:46:00 +0800

    Add files via upload

---
+++ 嘉晚饭再赢.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int	main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	//printf("sum = %d\n", sum);
+	printf("ºÍÎª %dÄó",sum);
+	return 0;
+}
+//¹Ø×¢¼ÎÍí·¹ß÷£¬¹Ø×¢¼ÎÍí·¹Ğ»Ğ»ß÷
